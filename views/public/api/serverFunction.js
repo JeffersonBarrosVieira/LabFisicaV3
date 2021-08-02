bcb22a37d7c3f62a3f014479994b9ad152af5448
jeffersonm03 <jefferson.negociom03@gmail.com>
2021-08-02 00:42:39 -0300

    serverFunction

--- views/public/api/serverFunction.js
+++ views/public/api/serverFunction.js
@@ -2,10 +2,10 @@ module.exports = (req, res) => {
   let assunto = req.body.assunto;
   let mensagem = req.body.mensagem;
   console.log({assunto, mensagem});
-  
-  res.json({
-    body: req.body,
-    query: req.query,
-    cookies: req.cookies,
-  });
+
+  // res.json({
+  //   body: req.body,
+  //   query: req.query,
+  //   cookies: req.cookies,
+  // });
 };
