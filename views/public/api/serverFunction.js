51e364009c38a8e430c08c34d593ad8e30d2d1cb
jeffersonm03 <jefferson.negociom03@gmail.com>
2021-08-02 00:51:47 -0300

    serverFunction

--- views/public/api/serverFunction.js
+++ views/public/api/serverFunction.js
@@ -1,10 +1,29 @@
 module.exports = (req, res) => {
   let assunto = req.body.assunto;
   let mensagem = req.body.mensagem;
+  let conn = false;
   console.log({assunto, mensagem});
+
+  async () => {
+    let uri = process.env.MONGO_URI;
+    let client = new MongoClient(uri);
+
+    try {
+        await client.connect();
+        console.log('Conected')
+        conn = true;
+        // await listDatabases(client, assunto, mensagem);
+
+    } catch (error) {
+        console.error(error);
+    } finally {
+        await client.close();
+    }
+  }
 
   res.json({
     body: req.body,
+    conected: conn,
     query: req.query,
     cookies: req.cookies,
   });
