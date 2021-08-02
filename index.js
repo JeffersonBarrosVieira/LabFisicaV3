21e02c91c6b3f8058abd97ea439ede5534236933
jeffersonm03 <jefferson.negociom03@gmail.com>
2021-08-01 22:04:00 -0300

    sendMessagemFinally

--- index.js
+++ index.js
@@ -26,37 +26,37 @@ require('dotenv/config')
     app.post('/send', (req, res) => {
         let assunto = req.body.assunto;
         let mensagem = req.body.mensagem;
-        enviarMensagem(assunto, mensagem).catch(console.error);;
+        // enviarMensagem(assunto, mensagem).catch(console.error);;
     })
 
 // Enviar mensagem
-function listDatabases(client, assunto, mensagem){
-    const result = await client.db("labfisica")
-    .collection("mensagens")
-    .insertOne({
-        assunto: `${assunto}`,
-        mensagem: `${mensagem}`,
-        data: new Date().toLocaleString("pt-BR")
-    });
-    console.log(`Mensagem inserida com id: ${ result.insertedId }`);
-}
+// function listDatabases(client, assunto, mensagem){
+//     const result = await client.db("labfisica")
+//     .collection("mensagens")
+//     .insertOne({
+//         assunto: `${assunto}`,
+//         mensagem: `${mensagem}`,
+//         data: new Date().toLocaleString("pt-BR")
+//     });
+//     console.log(`Mensagem inserida com id: ${ result.insertedId }`);
+// }
 
-async function enviarMensagem(assunto, mensagem){
+// async function enviarMensagem(assunto, mensagem){
 
-    const uri = process.env.MONGO_URI;
+//     const uri = process.env.MONGO_URI;
     
-    const client = new MongoClient(uri);
+//     const client = new MongoClient(uri);
 
-    try {
-        await client.connect();
-        await listDatabases(client, assunto, mensagem);
+//     try {
+//         await client.connect();
+//         await listDatabases(client, assunto, mensagem);
 
-    } catch (error) {
-        console.error(error);
-    } finally {
-        await client.close();
-    }
-};
+//     } catch (error) {
+//         console.error(error);
+//     } finally {
+//         await client.close();
+//     }
+// };
 
 // Execução local
     const port = process.env.PORT || 5000;
