86ff7019630e3933fc539b132b45435e3d66688a
jeffersonm03 <jefferson.negociom03@gmail.com>
2021-09-05 16:20:18 -0300

    plano-inclinado

--- public/js/vpython.js
+++ public/js/vpython.js
@@ -1,3 +1,13 @@
+/* - - - - - - -  funções  - - - - - - - */
+function multiplicarVetor(vetor, num) {
+    return vec(vetor.x * num, vetor.y * num, vetor.z * num)
+}
+
+function somarVetor(vetor1, vetor2) {
+    return vec(vetor1.x + vetor2.x, vetor1.y + vetor2.y, vetor1.z + vetor2.z)
+}
+
+
 /* - - - - - - -  CUBO  - - - - - - - */
 async function cubo() {
 
@@ -27,6 +37,8 @@ async function cubo() {
 window.__context = { glowscript_container: $("#cubo") };
 cubo();
 
+
+
 /* - - - - - - -  CILINDRO  - - - - - - - */
 async function cilindro() {
 
@@ -43,15 +55,6 @@ window.__context = { glowscript_container: $("#cilindro") };
 cilindro();
 
 
-
-/* - - - - - - -  funções  - - - - - - - */
-function multiplicarVetor(vetor, num) {
-    return vec(vetor.x * num, vetor.y * num, vetor.z * num)
-}
-
-function somarVetor(vetor1, vetor2) {
-    return vec(vetor1.x + vetor2.x, vetor1.y + vetor2.y, vetor1.z + vetor2.z)
-}
 
 /* - - - - - - -  TRAJETÓRIA  - - - - - - - */
 async function trajetoria() {
@@ -142,7 +145,68 @@ async function trajetoria() {
     return 0;
 }
 
-
-
 window.__context = { glowscript_container: $("#trajetoria") };
 trajetoria();
+
+
+
+async function planoInclinado() {
+    let scene = canvas();
+    scene.forward = vec(-2, -0.4, -2);
+    scene.range = 3;
+
+    let piso = box({
+        pos: vec(0, -0.025, 0),
+        size: vec(3, 0.05, 5),
+        texture: textures.gravel
+    })
+
+    let alfa = 30;
+    alfa *= Math.PI / 180;
+
+    let h = 1;
+    let rampa = box({
+        size: vec(1, 0.05, 2 * h / Math.sin(alfa)),
+        pos: vec(0, 0.025 + h, 0),
+        texture: textures.wood
+    });
+    rampa.rotate({ angle: alfa, axis: vec(1, 0, 0) });
+
+    let parede = box({
+        size: vec(1, 2 * h, 0.05),
+        pos: vec(0, h, -rampa.size.z * Math.cos(alfa) / 2),
+        texture: textures.wood
+    });
+
+    let dy = rampa.size.y * Math.cos(alfa) + rampa.pos.y + (h - 0.5 * Math.cos(alfa)) + 0.25 / Math.cos(alfa);
+    let dz = rampa.size.y * Math.sin(alfa) + rampa.pos.z + (-rampa.size.z * Math.cos(alfa) / 2 + 0.5 * Math.sin(alfa));
+
+
+    let g = 9.8;
+    let gx = g * Math.sin(alfa)
+
+    let bloco = box({
+        pos: vec(0, 0.25 + dy, 0 + dz),
+        size: vec(0.5, 0.5, 0.5),
+        color: color.cyan,
+        velocity: vec(0, 0, 0),
+        acceleration: vec(0, -gx * Math.sin(alfa), gx * Math.cos(alfa))
+    })
+    bloco.rotate({ angle: alfa, axis: vec(1, 0, 0) });
+
+    async function deslisar() {
+        let dt = 0.01;
+
+        while (true) {
+            await rate(100);
+            bloco.pos = somarVetor(bloco.pos, multiplicarVetor(bloco.velocity, dt));
+            bloco.velocity = somarVetor(bloco.velocity, multiplicarVetor(bloco.acceleration, dt));
+            if (bloco.pos.y < bloco.size.y / (2 * Math.cos(alfa))) break;
+        }
+    }
+    scene.camera.follow(bloco);
+    deslisar();
+}
+
+// window.__context = { glowscript_container: $("#plano-inclinado") };
+// planoInclinado();
