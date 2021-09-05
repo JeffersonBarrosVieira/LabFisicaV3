f26f57f9cb19baa3baa97a6b85ee99a8b5bee8ae
jeffersonm03 <jefferson.negociom03@gmail.com>
2021-09-05 14:04:02 -0300

    remocao-apple

--- public/js/apple.js
+++ public/js/apple.js
@@ -1,22 +1,22 @@
-let apple = document.querySelector("img[src='/img/apple.png']");
-let newton = document.querySelector("img[src='/img/newton.png']");
+// let apple = document.querySelector("img[src='/img/apple.png']");
+// let newton = document.querySelector("img[src='/img/newton.png']");
 
 
-// console.log(newton.getBoundingClientRect().y - apple.getBoundingClientRect().y);
+// // console.log(newton.getBoundingClientRect().y - apple.getBoundingClientRect().y);
 
-let newton0 = newton.getBoundingClientRect().y;
-let proporcao = newton0 / window.innerHeight;
-let esconder = false;
+// let newton0 = newton.getBoundingClientRect().y;
+// let proporcao = newton0 / window.innerHeight;
+// let esconder = false;
 
-document.getElementById('conteudo').addEventListener('scroll', () => {
+// document.getElementById('conteudo').addEventListener('scroll', () => {
     
-    // console.log((((newton0 - newton.getBoundingClientRect().y) / proporcao)/window.innerHeight)*200);
-    if(!esconder){
-        apple.style.marginTop = `${(newton0 - newton.getBoundingClientRect().y) + ((newton0 - newton.getBoundingClientRect().y)*1 / proporcao) + ((((newton0 - newton.getBoundingClientRect().y + 200) / proporcao)/window.innerHeight)*600)}px`;
+//     // console.log((((newton0 - newton.getBoundingClientRect().y) / proporcao)/window.innerHeight)*200);
+//     if(!esconder){
+//         apple.style.marginTop = `${(newton0 - newton.getBoundingClientRect().y) + ((newton0 - newton.getBoundingClientRect().y)*1 / proporcao) + ((((newton0 - newton.getBoundingClientRect().y + 200) / proporcao)/window.innerHeight)*600)}px`;
 
-        if( newton.getBoundingClientRect().y < apple.getBoundingClientRect().y ){
-            esconder = true;
-            apple.style.visibility = 'hidden';
-        }
-    }
-})
+//         if( newton.getBoundingClientRect().y < apple.getBoundingClientRect().y ){
+//             esconder = true;
+//             apple.style.visibility = 'hidden';
+//         }
+//     }
+// })
